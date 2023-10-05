d8a2a7d20039b1190c277a7a2b3d184306cb9bda
Mani Movassagh <mani.mgh@gmail.com>
2023-10-05 22:49:37 +0200

    final

--- src/blockFactory/blockFactory.ts
+++ src/blockFactory/blockFactory.ts
@@ -1,5 +1,6 @@
 import { AppDataSource } from "../data-source";
 import { Block } from "../entity/block";
+import { genesisBlock } from "./genesis";
 
 export async function addBlockFactory(amount:number) {
     const currentBlockChain = await AppDataSource.manager.find(Block);
--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,15 @@ import { logger } from "./logger/logger";
 
 
 AppDataSource.initialize().then(async () => {
+
     await AppDataSource.manager.save(genesisBlock)
     await addBlockFactory(3);
     await addBlockFactory(4.4);
     await addBlockFactory(22.45);
     await addBlockFactory(44.45);
-   
-   console.log(await AppDataSource.manager.find(Block));
-    
+
+    console.log(await AppDataSource.manager.find(Block));
+
 }).catch(error => logger.warn(error))
 
 
