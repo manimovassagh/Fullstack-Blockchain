812dde36dfe8a7a7c6aeb4590dfa58abf8182179
Mani Movassagh <mani.mgh@gmail.com>
2023-10-10 20:53:14 +0200

    docs

--- src/router/getAll.ts
+++ src/router/getAll.ts
@@ -5,7 +5,9 @@ import { Block } from "../entity/block";
 const express = require("express")
 
 export const showBlockChain = express.Router()
-
+/**
+ * get all Blocks and send to frontend
+ */
 showBlockChain.get('/', async (_req: Request, res: Response) => {
     const fetch = await AppDataSource.manager.find(Block);
     res.json(fetch)
