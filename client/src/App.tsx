43d2a14baea12efcd16c11648e2ef5b8bbc247fc
Mani Movassagh <mani.mgh@gmail.com>
2023-10-17 02:06:44 +0200

    cleanup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,11 @@
 import axios from 'axios';
 import './App.css';
 import Blockchain from './components/Blockchain';
-import LoginButton from './components/LoginButton';
 
 function App() {
   axios.defaults.headers.common['Authorization'] = localStorage.getItem("token") as string;
   return (
     <>
-      <LoginButton></LoginButton>
       <Blockchain></Blockchain>
     </>
   )
